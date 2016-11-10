bb37970c096898ea04004e8efa6c6868483bea9d
cruxyoung <cruxyoung@hotmail.com>
2016-11-10 17:38:16 +1100

    added download function by using you-get or youtube-dl

--- blog.py
+++ blog.py
@@ -1,17 +1,32 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, url_for,request
+
+from subprocess import call
+
+
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
+@app.route('/base_test')
+def base_test_page():
     return render_template('base.html')
 
 
-@app.route('/extends')
+@app.route('/')
 def extend():
     return render_template('index.html')
 
+@app.route('/download',methods=['GET','POST'])
+def download_page():
+    if request.method == 'POST':
+        download_url = request.form.get('address','')
+        # command = "youtube-dl -ict " + download_url
+        command = "you-get " + download_url
+        call(command.split(), shell=False)
+        command = ""
+    return render_template('download.html')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True )
